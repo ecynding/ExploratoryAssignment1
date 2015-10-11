f589a0acf9924c110c1b12a3d7ea6c89a35b5f30
ecynding <yixding@gmail.com>
2015-10-11 21:58:55 +0800

    working directory
    updated comment on working directory

--- plot4.r
+++ plot4.r
@@ -1,8 +1,8 @@
 # Project 1
 
 # Initializing
-# please move into the folder in which you unzipped the dataset (setwd())
-setwd('C://Users//Tijl//Documents//Github//edaCoursera')
+# please put the unzipped data file into your working directory
+
 dt=read.csv('household_power_consumption.txt',header=T, sep=';') # read the textfile which is formatted as a csv
 dt$Datetime = paste(as.character(dt[,1]) , dt[,2])  # merge date and time into a single column (all string variables)
 dt[,1]=as.Date(dt$Date,'%d/%m/%Y') # convert the Date column's datatype from string to date 
